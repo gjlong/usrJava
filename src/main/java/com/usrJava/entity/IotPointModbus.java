20ceead46a62160fc9eb2227661722534be19136
gjlong <2351770805@qq.com>
2021-02-11 14:31:08 +0800

    补充注释。

--- src/main/java/com/usrJava/entity/IotPointModbus.java
+++ src/main/java/com/usrJava/entity/IotPointModbus.java
@@ -8,10 +8,10 @@ package com.usrJava.entity;
  * @date 2020年05月16日 16:42
  */
 public class IotPointModbus {
-    private int dataid;
-    private String itemId;
-    private int writeRead;
-    private int relId;
+    private int dataid;//数据点id
+    private String itemId;//itemId
+    private int writeRead;//读写类型：0：只读 1：读写
+    private int relId;//关联id
 
     public int getDataid() {
         return dataid;
