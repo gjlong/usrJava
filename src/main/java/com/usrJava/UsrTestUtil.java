888fc33f3fc1803fcaf03c884e004d00405e1c72
gjlong <2351770805@qq.com>
2021-03-19 18:31:46 +0800

    修改日志调试文件。

--- src/main/java/com/usrJava/UsrTestUtil.java
+++ src/main/java/com/usrJava/UsrTestUtil.java
@@ -17,5 +17,12 @@ public class UsrTestUtil {
         log.info("测试log info");
         log.error("测试log error");
         log.info(logContent);
+
+        double y=1.0;
+        for(int i=0;i<=1000;i++){
+            double π=3*Math.pow(2, i)*y;
+            System.out.println("第"+i+"次切割,为正"+(6+6*i)+"边形，圆周率π≈"+π);
+            y=Math.sqrt(2-Math.sqrt(4-y*y));
+        }
     }
 }
