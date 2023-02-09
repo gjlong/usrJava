5813a0b5238a9d27ad83985341b700eb3ffa28de
gjlong <2351770805@qq.com>
2023-02-09 22:06:24 +0800

    修改搜索页面

--- src/main/java/com/usrJava/UsrTestUtil.java
+++ src/main/java/com/usrJava/UsrTestUtil.java
@@ -339,6 +339,8 @@ public class UsrTestUtil {
             HtmlPage htmlPage = webClient.getPage("https://baidu.com/");
             webClient.waitForBackgroundJavaScript(1000);//等待异步js执行完毕，否则不会生成数据dom格式
             System.out.println(htmlPage.asXml());
+            //输出网页内容
+            
 
         } catch (IOException e) {
             e.printStackTrace();
