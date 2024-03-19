a464f216be14a087480c8fcfaba98913b8763873
gjlong <2351770805@qq.com>
2024-03-19 11:52:03 +0800

    修改测试文件

--- src/main/java/com/usrJava/TestLinuxProject.java
+++ src/main/java/com/usrJava/TestLinuxProject.java
@@ -1,9 +1,25 @@
-package com.usrJava;/**
-**@ClassName: TestLinuxProject
-**@Package com.usrJava
-**@Description: 添加描述
-**@author Harry
-**@date 2024年03月19日 上午11:44
-**/
+package com.usrJava;
+
+import com.testJavaseMybatis.service.UrlsService;
+import com.testJavaseMybatis.service.impl.UrlsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * *@ClassName: TestLinuxProject
+ * *@Package com.usrJava
+ * *@Description: 添加描述
+ * *@author Harry
+ * *@date 2024年03月19日 上午11:44
+ **/
 public class TestLinuxProject {
+    public static Logger log = LoggerFactory.getLogger(TestLinuxProject.class);
+    private UrlsService urlsService = new UrlsServiceImpl();
+
+    public void testLog(String logContent) {
+        log.debug("测试log debug");
+        log.info("测试log info");
+        log.error("测试log error");
+        log.info(logContent);
+    }
 }
