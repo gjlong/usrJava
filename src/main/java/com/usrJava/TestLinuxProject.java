0c1eb80ac963e33baa48d2c97191539d6d9d25e0
gjlong <2351770805@qq.com>
2024-03-22 11:19:00 +0800

    增加jwt

--- src/main/java/com/usrJava/TestLinuxProject.java
+++ src/main/java/com/usrJava/TestLinuxProject.java
@@ -1,10 +1,16 @@
 package com.usrJava;
 
+import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithm;
 import com.testJavaseMybatis.service.UrlsService;
 import com.testJavaseMybatis.service.impl.UrlsServiceImpl;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
 
 /**
  * *@ClassName: TestLinuxProject
@@ -24,7 +30,36 @@ public class TestLinuxProject {
         log.info(logContent);
     }
 
-    public static void main(String[] args) {
+    private String createJWT(String id, String issuer, String subject, long ttlMillis) {
+
+//The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+//We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(apiKey.getSecret());
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+//if it has been specified, let's add the expiration
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+//Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+
+        public static void main(String[] args) {
         System.out.println("run");
         //生成token
         String testString="test";
