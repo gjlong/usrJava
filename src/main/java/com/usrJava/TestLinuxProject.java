20ee8a44d6253dee55848104db6d69df01c31291
gjlong <2351770805@qq.com>
2024-05-08 14:14:21 +0800

    取消测试jwt代码

--- src/main/java/com/usrJava/TestLinuxProject.java
+++ src/main/java/com/usrJava/TestLinuxProject.java
@@ -11,6 +11,7 @@ import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * *@ClassName: TestLinuxProject
@@ -28,42 +29,8 @@ public class TestLinuxProject {
         log.info("测试log info");
         log.error("测试log error");
         log.info(logContent);
+        System.out.println(UUID.randomUUID().toString());
     }
 
-    private String createJWT(String id, String issuer, String subject, long ttlMillis) {
 
-//The JWT signature algorithm we will be using to sign the token
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
-        long nowMillis = System.currentTimeMillis();
-        Date now = new Date(nowMillis);
-
-//We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(apiKey.getSecret());
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-
-        //Let's set the JWT Claims
-        JwtBuilder builder = Jwts.builder().setId(id)
-                .setIssuedAt(now)
-                .setSubject(subject)
-                .setIssuer(issuer)
-                .signWith(signatureAlgorithm, signingKey);
-
-//if it has been specified, let's add the expiration
-        if (ttlMillis >= 0) {
-            long expMillis = nowMillis + ttlMillis;
-            Date exp = new Date(expMillis);
-            builder.setExpiration(exp);
-        }
-
-//Builds the JWT and serializes it to a compact, URL-safe string
-        return builder.compact();
-
-        public static void main(String[] args) {
-        System.out.println("run");
-        //生成token
-        String testString="test";
-        System.out.println(DigestUtils.sha1Hex(testString));
-
-    }
 }
